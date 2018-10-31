e3973d94200a183d1fb2cbbf89720d05c26d9ea8
owenwilson <owenwilson76467963@gmail.com>
2018-10-31 08:50:03 -0400

    update main

--- src/org/exampleabstrac/Principal.java
+++ src/org/exampleabstrac/Principal.java
@@ -3,7 +3,7 @@ package org.exampleabstrac;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Planta planta = new Planta();
 		AnimalCarnivoro animalC = new AnimalCarnivoro();
 		
